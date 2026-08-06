047de631b1a5a6c8614b77a798655402c35a2bd8
ferney2281 <ferney.chica@upb.edu.co>
2026-08-06 13:22:00 -0500

    Condicionales simples dobles

---
+++ 4.CondicionalesSimplesDobles/4.CondicionalesSimpleDoble/4.CondicionalesSimpleDoble/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _4.CondicionalesSimpleDoble
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ESTRUCTURAS DE CONTROL CONDICIONALES
+            //Simples
+            //1.	Crea un algoritmo que lea la edad de un usuario, si el usuario es mayor de 18, se debe mostrar el siguiente mensaje: “Bienvenido a mi sitio web”.
+            /*byte edad = 0;
+            Console.WriteLine("Ingrese su edad");
+            edad = Convert.ToByte( Console.ReadLine() );
+            if(edad >= 18)
+            {
+                //Si se cumple la condición
+                Console.WriteLine("Bienvenido a mi sitio web");
+            }*/
+            //Crear un algoritmo que permita ingresar el nombre y el sueldo de una persona, si el sueldo supera los 3000 pesos mostrar el mensaje en pantalla con el nombre de la persona, indicando que debe abonar impuestos.
+
+            //Condicionales dobles
+            //Crea un algoritmo que lea la edad de un usuario, si el usuario es mayor de 18, se debe mostrar el siguiente mensaje: “Bienvenido a mi sitio web”. De lo contrario, mostrar el mensaje “No es apto para el contenido de este sitio web”.  
+            byte edad = 0;
+            Console.WriteLine("Ingrese su edad");
+            edad = Convert.ToByte(Console.ReadLine());
+            if (edad >= 18)
+            {
+                //Si se cumple la condición
+                Console.WriteLine("Bienvenido a mi sitio web");
+            }
+            else 
+            {
+                //Si NO se cumple la condición
+                Console.WriteLine("No es apto para el contenido del sitio web");
+            }
+        }
+    }
+}
